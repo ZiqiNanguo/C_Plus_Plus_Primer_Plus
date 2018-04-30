75c9cfb65c8c95a597d18b8d9c1735240395627a
ZiqiNanguo <33706594+ZiqiNanguo@users.noreply.github.com>
2018-04-30 13:23:30 +0800

    Init_commit
    This is the first time to push the code of C++ Primer Plus

---
+++ chapter3/3.4/hexoct2.cpp
@@ -0,0 +1,17 @@
+// hexoct2.cpp -- display values in  hex and octal
+#include <iostream>
+int main()
+{
+	using namespace std;
+	int chest = 42;
+	int waist = 42;
+	int inseam = 42;
+	
+	cout << "Monsieur cuts a striking figure!" << endl;
+	cout << "chest = " << chest << " (42 in decimal)" << endl;
+	cout << hex;           // manipulator for changing number base
+	cout << "waist = " << waist << " (0x42 in hex)" << endl;
+	cout << oct;           // manipulator for changing number base
+	cout << "inseam = " << inseam << " (042 in octal)" << endl;
+	return 0;
+}
